7ff90c05350169da014cc829e90ba03766413ea5
jslaslo <125125092+jslaslo@users.noreply.github.com>
2023-04-01 10:28:30 +0200

    TicTacToeGame
    TicTacToeGame

---
+++ Program.cs
@@ -0,0 +1,62 @@
+﻿using HomeWork_.TicTacToeGame;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data.SqlTypes;
+using System.IO;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+
+namespace HomeWork_.TicTacToeGame
+{   
+    
+    class Program
+    {
+        private static readonly TicTacToeGame Game = new TicTacToeGame();
+        static void Main(string[] args) 
+        {
+            Console.WriteLine(GetPrintState());
+            while (Game.GetWinner() == Winner.GameIsNotOver)
+            {
+                int index = int.Parse(Console.ReadLine());
+                Console.Clear();
+                Game.MakeMove(index);
+
+                Console.WriteLine();
+                Console.WriteLine(GetPrintState());
+
+            }
+            Console.WriteLine($"Победа {Game.GetWinner()}");
+        }                      
+        static string GetPrintState()
+        {
+            var state = new StringBuilder();
+            for (int i = 1; i <= 7; i += 3)
+            {
+                state.AppendLine("     |     |     |")
+                     .AppendLine($"  {GetPrintChar(i)}  |  {GetPrintChar(i + 1)}  |  {GetPrintChar(i + 2)}  |")
+                     .AppendLine("_____|_____|_____|");
+            }
+            return state.ToString();
+        }
+        static string GetPrintChar(int index)
+        {
+            State state = Game.GetState(index);
+            if (state == State.Unset)
+            {
+                return index.ToString();
+            }
+            return state == State.Cross ? "X" : "O";
+        }      
+        
+    }
+}
+
+
+
+
+
---
+++ TicTacToeGame/TicTacToeGame.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HomeWork_.TicTacToeGame
+{
+    enum State
+    {
+        Unset,
+        Cross,
+        Zero
+    }
+    public enum Winner
+    {
+        Crosses,
+        Zero,
+        Draw,
+        GameIsNotOver
+    }
+    class TicTacToeGame
+    {
+        private readonly State[] field = new State[9];
+        public int StrokeCounter { get; private set; }
+
+        public TicTacToeGame()
+        {
+            for (int i = 0; i < field.Length; i++)
+            {
+                field[i] = State.Unset;
+            }
+        }
+        public void MakeMove(int index)
+        {
+            field[index - 1] = StrokeCounter % 2 == 0 ? State.Cross : State.Zero;
+
+            StrokeCounter++;
+        }
+        public State GetState(int index)
+        {
+            return field[index - 1];
+        }
+
+        public Winner GetWinner()
+        {
+            return GetWinner(1, 4, 7, 2, 5, 8, 3, 6, 9,
+                             1, 2, 3, 4, 5, 6, 7, 8, 9,
+                             1, 5, 9, 3, 5, 7);
+        }
+        public Winner GetWinner(params int[] indexes)
+        {
+            for (int i = 0; i < indexes.Length; i += 3)
+            {
+                bool same = AreSame(indexes[i], indexes[i + 1], indexes[i + 2]);
+                if (same)
+                {
+                    State state = GetState(indexes[i]);
+                    if (state != State.Unset)
+                    {
+                        return state == State.Cross ? Winner.Crosses : Winner.Zero;
+                    }
+                }
+            }
+            if (StrokeCounter < 9)
+            {
+                return Winner.GameIsNotOver;
+            }
+            return Winner.Draw;
+        }
+        private bool AreSame(int a, int b, int c)
+        {
+            return GetState(a) == GetState(b) && GetState(a) == GetState(c);
+        }
+    }
+}
